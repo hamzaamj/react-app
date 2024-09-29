3ad7ec952f47f2fc83af16324017d887f76fd14f
hamza.amjad <hamza.amjad531@gmail.com>
2024-09-30 00:52:50 +0500

    react project with props work done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from "react";
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar title="ReactApp" aboutText="About ReactApp"/>
+    </>
+  );
+}
+export default App;
